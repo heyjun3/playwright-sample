1d12c15fb848d3851e4cacd77e647087223a6e09
heyjun3 <t3k2g5kako@yahoo.co.jp>
2024-02-25 15:21:25 +0900

    fix

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -7,16 +7,20 @@ test('wizard', async () => {
   const context = await browser.newContext({
     userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/109.0'
   })
-  await context.addCookies([{ name: 's', url: baseURL, value: process.env.COOKIE as string }])
+  await context.addCookies([{ name: 's', url: baseURL, value: process.env.COOKIE ?? "" }])
   const page = await context.newPage()
-  for (let i = 1; i < 100; i++) {
+  for (let i = 0; i < 50; i++) {
     const query = {
       page: i,
       paging_order: 2,
     }
+    log("run page", query)
     await page.goto(new URL(`${baseURL}?${querystring.stringify(query)}`).toString());
     const links = page.locator('.link-area.needsclick.append-anchor')
     const count = await links.count()
+    if (count <= 0) {
+      throw new Error("failed open list page")
+    }
     const url: string[] = []
     for (let j = 0; j < count; j++) {
       const path = await links.nth(j).getAttribute('href')
