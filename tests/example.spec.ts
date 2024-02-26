2df41ce54e60a18a781b99c9c106219bb2df1485
heyjun3 <t3k2g5kako@yahoo.co.jp>
2024-02-26 20:01:13 +0900

    fix

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,4 +1,5 @@
-import { test, chromium } from '@playwright/test';
+import { test, chromium, errors }from '@playwright/test';
+import { error } from 'node:console';
 import querystring from 'node:querystring'
 
 test('wizard', async () => {
@@ -15,24 +16,40 @@ test('wizard', async () => {
       paging_order: 2,
     }
     log("run page", query)
-    await page.goto(new URL(`${baseURL}?${querystring.stringify(query)}`).toString());
+    const url = new URL(`${baseURL}?${querystring.stringify(query)}`).toString()
+    try {
+      await page.goto(url, { timeout: 10 * 1000});
+    } catch (err) {
+      if (err instanceof errors.TimeoutError) {
+        log("time out err", url)
+        continue
+      }
+    }
     const links = page.locator('.link-area.needsclick.append-anchor')
     const count = await links.count()
     if (count <= 0) {
       throw new Error("failed open list page")
     }
-    const url: string[] = []
+    const details: string[] = []
     for (let j = 0; j < count; j++) {
       const path = await links.nth(j).getAttribute('href')
       if (path == null) {
         continue
       }
-      url.push(new URL(path, baseURL).toString())
+      details.push(new URL(path, baseURL).toString())
     }
-    log("url length", url.length)
-    for (const u of url) {
+    log("url length", details.length)
+    for (const u of details) {
       log("request url", u)
-      await page.goto(u, { timeout: 3 * 1000 })
+      try {
+        await page.goto(u, { timeout: 4 * 1000 })
+      } catch (err) {
+        if (err instanceof errors.TimeoutError){
+          log("time out error", u)
+          continue
+        }
+        throw err
+      }
       await new Promise((resolve) => setTimeout(resolve, Math.floor(Math.random() * 3000)))
     }
   }
